54b69f9c2b7148cf7ff5cb6a87197ddc709f6615
Victor Spiewak <victorspiewak@hotmail.com>
2020-02-07 22:38:56 -0500

    MNIST Initialize repo

---
+++ MNIST_Parser.py
@@ -0,0 +1,106 @@
+import time, struct as st, numpy as np, pickle as pk
+ 
+def main():
+	net = network(784,[784,16,10])
+	images = 20
+	labels_l = getLabels('train-labels.idx1-ubyte')
+	images_l = getImages('train-images.idx3-ubyte',images)
+	
+	for i in range(0,images):
+		net.update[0] += sigmoid(np.dot(net.weights[0],images_l[i]) + net.bias[0])
+		for j in range(1,net.numLayers-1):
+			net.update[j] += sigmoid(np.dot(net.weights[j],net.weights[j+1]) + net.bias[j])
+            
+    
+class network(object):
+    def __init__(self,size,neurons):
+        self.numLayers = len(neurons)
+        self.bias = [np.random.randn(x,1) for x in neurons[1:]]
+        self.weights = [np.random.randn(x,y) for x, y in zip(neurons[:-1],neurons[1:])]
+        self.update = [np.zeros((x,1)) for x in neurons[1:]]
+    
+def toHex(s):
+    lst = []
+    for ch in s:
+        hv = hex(ord(ch)).replace('0x', '')
+        if len(hv) == 1:
+            hv = '0'+hv
+        lst.append(hv)
+    return reduce(lambda x,y:x+y, lst)
+	
+def sigmoid(x):
+	return 1.0/(1.0 + np.exp(-x))
+	
+def dsigmoid(x):
+	# d/dx (1 + e^(-x))^-1
+	# -1(1 + e^(-x))^-2 * (-e^(-x))
+	# e^(-x) / (1 + e^(-x))^2
+	# e^(-x) * sigmoid(x)^2
+	return np.exp(-x) * sigmoid(x)**2
+	
+def percepitron(value,bias):
+	if value > bias:
+		return 1
+	return 0
+
+def getLabels(filename):
+	labels_l = []
+	print 'Getting labels'
+	with open(filename,'rb') as f:
+		chunk = f.read(8)
+		MSB = st.unpack('>I',chunk[0:4])[0]
+		labels = st.unpack('>I',chunk[4:8])[0]
+		chunk = f.read(labels)
+		for i in range(0,labels):
+			labels_l.append(st.unpack('B',chunk[0+i:1+i])[0])
+	print 'Done getting labels'
+	return labels_l
+
+def printNum(chunk): 
+	for i in range(0,28):
+		print toHex(chunk[28*i:28*(i+1)])
+	print ''
+	
+def getImages(filename,images_n):
+	print 'Getting images'
+	with open(filename,'rb') as f:
+		chunk = f.read(16)
+		MSB = st.unpack('>I',chunk[0:4])[0]
+		images = st.unpack('>I',chunk[4:8])[0]
+		xDim = st.unpack('>I',chunk[8:12])[0]
+		yDim = st.unpack('>I',chunk[12:16])[0]
+		imageSize = xDim * yDim
+		images_l = np.zeros((images,imageSize))
+		count = 0
+		for i in range(0,images_n):
+			chunk = f.read(imageSize)
+			#printNum(chunk)
+			for j in range(0,imageSize):
+				images_l[i,j] = st.unpack('B',chunk[j:(j+1)])[0]
+	print 'Done getting images'
+	return images_l
+
+def layer(weights,bias,image,numNeurons):
+	neurons = np.zeros((numNeurons,1))
+	for i in range(0,numNeurons):
+		neurons[i] = sigmoid(np.dot(weights[i],image)/numNeurons + bias[i])
+	return neurons
+	
+def right(i,lables):
+	if i == lables:
+		return 1
+	else:
+		return 0	
+	
+def backPropagation():
+	cat = 1
+	
+if __name__ == '__main__':
+    main()
+    raw_input("\nDone...press ENTER to exit")
+	
+	
+	
+	
+	
+	
