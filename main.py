97785535d5c3730076b94f909d13e5ae6bc85a95
Victor Spiewak <victorspiewak@hotmail.com>
2020-10-29 20:55:13 -0400

    Added other Neural Networks

--- main.py
+++ main.py
@@ -1,54 +1,182 @@
+# Python Libraries
+from time import process_time as ptime
+import argparse
+
+# PyTorch
 import torch
-import torch.utils.data as td
-import torch.optim as optim
+import torch.nn as nn
+import torchvision.transforms as transforms
+import torchvision.datasets as dsets
+from torchsummary import summary
 
-from mnist import *
+# Networks
+from models import *
 
-# Set the device for torch as GPU or CPU
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+if __name__ == "__main__":
+    # Parse the input arguments of main.py
+    parser = argparse.ArgumentParser(description='ESP-VO Model implemented in PyTorch')
 
-if __name__ == '__main__':
-    model = MNIST()
-    model.to(device)
+    # General arguments
+    parser.add_argument('--model', default="lstm", type=str, help='Chooses the model to be run')
+    parser.add_argument('--batch_size', default=100, type=int, help='Number of images per batch')
+    parser.add_argument('--iter', default=6000, type=int, help='Number of iterations to train on')
+    parser.add_argument('--device', default=torch.device("cuda:0" if torch.cuda.is_available() else "cpu"), \
+        type=str, help='Device to run PyTorch with')
 
-    batch_size = 64
+    parser.add_argument('--progress', default=1, type=int, help='Shows progress bar for training')
+    parser.add_argument('--manual_seed', default=0, type=int, help='Set the manual seed for deterministic results')
 
-    train_loader = td.DataLoader(MNISTDataset('train-labels.idx1-ubyte', 'train-images.idx3-ubyte'), \
-        batch_size=batch_size, shuffle=True, drop_last=True)
+    args = parser.parse_args()
 
-    # Set the model to train
-    model.train()
-    model.training = True
 
-    # Set the loss/criterion function
-    criterion = torch.nn.MSELoss()
+    '''
+    STEP 1: LOADING DATASET
+    '''
+    train_dataset = dsets.MNIST(root='./', 
+                                train=True, 
+                                transform=transforms.ToTensor(),
+                                download=False)
 
-    # Set the optimizer
-    optimizer = optim.Adam(model.parameters(), lr=0.001, betas=(0.9, 0.999)) #lr = 0.001, B1 = 0.9 B1 = 0.999 Epoch = 200, Dropout, early stopping (see: https://stackoverflow.com/questions/2976452/whats-is-the-difference-between-train-validation-and-test-set-in-neural-netwo), and incremental training techniques are introduced, uses pretrained FlowNet model CNN (Dosovitskiy 2015)    
+    test_dataset = dsets.MNIST(root='./', 
+                            train=False, 
+                            transform=transforms.ToTensor())
 
-    # Loop through all the training data
-    for batch_idx, (imgs, labels, index) in enumerate(train_loader):
-        optimizer.zero_grad()   # zero the gradient buffers
-        
-        # Print data variables
-        # print("Images:", imgs.shape)
-        # print("Labels:", labels.shape)
-        # print("Batch indexes:", index)
+    '''
+    STEP 2: MAKING DATASET ITERABLE
+    '''
+    batch_size = args.batch_size
+    n_iters = args.iter
+    num_epochs = n_iters / (len(train_dataset) / batch_size)
+    num_epochs = int(num_epochs)
 
-        # Evaluate the batch
-        est_labels = model(imgs.to(device))
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
+                                            batch_size=batch_size, 
+                                            shuffle=True)
 
-        # Print results
-        # print(est_labels)
-        # print(labels)
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset, 
+                                            batch_size=batch_size, 
+                                            shuffle=False)
 
-        # Calculate the loss of the batch
-        loss = criterion(est_labels, labels.to(device))
 
-        # Compute gradient and do  optimizer step
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+    '''
+    STEP 3: INSTANTIATE MODEL CLASS
+    '''
+    input_dim = 28
+    hidden_dim = 100
+    layer_dim = 3  # ONLY CHANGE IS HERE FROM ONE LAYER TO TWO LAYER
+    output_dim = 10
 
-        # Print the loss
-        print("loss:", loss.item())
+    modelname = args.model
+
+    print("\n==========================================================================================")
+    print(f"Loading model {modelname}".center(90))
+    print("==========================================================================================")
+
+    if (modelname == "lstm"):
+        model = LSTMModel(input_dim, hidden_dim, layer_dim, output_dim)
+        use_seq = 1
+    elif (modelname == "lstmcell"):
+        model = LSTMCellModel(input_dim, hidden_dim, layer_dim, output_dim)
+        use_seq = 0
+    elif (modelname == "rnn"):
+        model = RNNModel(input_dim, hidden_dim, layer_dim, output_dim)
+        use_seq = 1
+    elif (modelname == "cnn"):
+        model = CNNModel()
+        use_seq = 0
+    elif (modelname == "lenet"):
+        model = lenet()
+        use_seq = 0
+
+    # Move model to device
+    model.to(args.device)
+
+    # Print a Summary of the model
+    if args.summary:
+        summary(model, (28, 28), device="cuda")
+
+    '''
+    STEP 4: INSTANTIATE LOSS CLASS
+    '''
+    criterion = nn.CrossEntropyLoss()
+
+    '''
+    STEP 5: INSTANTIATE OPTIMIZER CLASS
+    '''
+    learning_rate = 0.1
+    #learning_rate = 0.01
+
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+    '''
+    STEP 6: TRAIN THE MODEL
+    '''
+
+    # Number of steps to unroll
+    seq_dim = 28
+
+    iter = 0
+    print(num_epochs)
+    for epoch in range(num_epochs):
+        for i, (images, labels) in enumerate(train_loader):
+            # Load images as Variable
+            # (batch_size, seq_dim, input_dim)
+            # (100 x 1 x 28 x 28)
+            if use_seq:
+                images = images.view(-1, seq_dim, input_dim).requires_grad_().to(device)
+            else:
+                images = images.requires_grad_().to(device)
+            # (100 x 28 x 28)
+
+            labels = labels.to(device)
+
+            # Clear gradients w.r.t. parameters
+            optimizer.zero_grad()
+
+            # Forward pass to get output/logits
+            # outputs.size() --> 100, 10
+            outputs = model(images)
+
+            # Calculate Loss: softmax --> cross entropy loss
+            loss = criterion(outputs, labels)
+
+            # Getting gradients w.r.t. parameters
+            loss.backward()
+
+            # Updating parameters
+            optimizer.step()
+
+            iter += 1
+
+            if iter % 500 == 0:
+                # Calculate Accuracy
+                correct = 0
+                total = 0
+                # Iterate through test dataset
+                for images, labels in test_loader:
+                    if use_seq:
+                        images = images.view(-1, seq_dim, input_dim).to(device)
+                    else:
+                        images = images.to(device)
+                    
+                    labels = labels.to(device)
+
+                    # Forward pass only to get logits/output
+                    outputs = model(images)
+
+                    # Get predictions from the maximum value
+                    _, predicted = torch.max(outputs.data, 1)
+
+                    # Total number of labels
+                    total += labels.size(0)
+
+                    # Total correct predictions
+                    if torch.cuda.is_available():
+                        correct += (predicted.cpu() == labels.cpu()).sum()
+                    else:
+                        correct += (predicted == labels).sum()
+
+                accuracy = 100 * correct // total
+
+                # Print Loss
+                print('Iteration: {}. Loss: {}. Accuracy: {}'.format(iter, loss.item(), accuracy))
---
+++ models.py
@@ -0,0 +1,254 @@
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+import torchvision.datasets as dsets
+import torch.nn.functional as F
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Note: ASCI generator: http://www.patorjk.com/software/taag/#p=display&v=3&f=ANSI%20Regular&t
+
+# ██      ███████ ████████ ███    ███ 
+# ██      ██         ██    ████  ████ 
+# ██      ███████    ██    ██ ████ ██ 
+# ██           ██    ██    ██  ██  ██ 
+# ███████ ███████    ██    ██      ██
+
+class LSTMModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
+        super(LSTMModel, self).__init__()
+        # Hidden dimensions
+        self.hidden_dim = hidden_dim
+
+        # Number of hidden layers
+        self.layer_dim = layer_dim
+
+        # Building your LSTM
+        # batch_first=True causes input/output tensors to be of shape
+        # (batch_dim, seq_dim, feature_dim)
+        self.lstm = nn.LSTM(input_dim, hidden_dim, layer_dim, batch_first=True)
+
+        # Readout layer
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        # Initialize hidden state with zeros
+        #######################
+        #  USE GPU FOR MODEL  #
+        #######################
+        h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_().to(device)
+
+        # Initialize cell state
+        c0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_().to(device)
+
+        # One time step
+        out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
+
+        # Index hidden state of last time step
+        # out.size() --> 100, 28, 100
+        # out[:, -1, :] --> 100, 100 --> just want last time step hidden states! 
+        out = self.fc(out[:, -1, :]) 
+        # out.size() --> 100, 10
+        return out
+
+# ██      ███████ ████████ ███    ███      ██████ ███████ ██      ██      
+# ██      ██         ██    ████  ████     ██      ██      ██      ██      
+# ██      ███████    ██    ██ ████ ██     ██      █████   ██      ██      
+# ██           ██    ██    ██  ██  ██     ██      ██      ██      ██      
+# ███████ ███████    ██    ██      ██      ██████ ███████ ███████ ███████
+
+class LSTMCellModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
+        super(LSTMCellModel, self).__init__()
+        # Hidden dimensions
+        self.hidden_dim = hidden_dim
+
+        # Number of hidden layers
+        self.layer_dim = layer_dim
+
+        # Building your LSTM
+        # batch_first=True causes input/output tensors to be of shape
+        # (batch_dim, seq_dim, feature_dim)
+        self.lstmcell = []
+        for _ in range(self.layer_dim):
+            self.lstmcell.append(nn.LSTMCell(input_dim, hidden_dim))
+
+        # Readout layer
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        # Initialize hidden state with zeros
+        #######################
+        #  USE GPU FOR MODEL  #
+        #######################
+        hx = torch.zeros(x.size(0), self.hidden_dim).requires_grad_().to(device)
+
+        # Initialize cell state
+        cx = torch.zeros(x.size(0), self.hidden_dim).requires_grad_().to(device)
+
+        # One time step
+        out = []
+        for i in range(self.layer_dim):
+            hx, cx = self.lstmcell[i](x[i], (hx.detach(), cx.detach()))
+            out.append(hx)
+
+        # Index hidden state of last time step
+        # out.size() --> 100, 28, 100
+        # out[:, -1, :] --> 100, 100 --> just want last time step hidden states! 
+        out = self.fc(out[:, -1, :]) 
+        # out.size() --> 100, 10
+        return out
+
+# ██████  ███    ██ ███    ██ 
+# ██   ██ ████   ██ ████   ██ 
+# ██████  ██ ██  ██ ██ ██  ██ 
+# ██   ██ ██  ██ ██ ██  ██ ██ 
+# ██   ██ ██   ████ ██   ████
+
+class RNNModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
+        super(RNNModel, self).__init__()
+        # Hidden dimensions
+        self.hidden_dim = hidden_dim
+
+        # Number of hidden layers
+        self.layer_dim = layer_dim
+
+        # Building your RNN
+        # batch_first=True causes input/output tensors to be of shape
+        # (batch_dim, seq_dim, feature_dim)
+        self.rnn = nn.RNN(input_dim, hidden_dim, layer_dim, batch_first=True, nonlinearity='tanh')
+
+        # Readout layer
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        # Initialize hidden state with zeros
+        #######################
+        #  USE GPU FOR MODEL  #
+        #######################
+        h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).to(device)
+
+        # One time step
+        # We need to detach the hidden state to prevent exploding/vanishing gradients
+        # This is part of truncated backpropagation through time (BPTT)
+        out, hn = self.rnn(x, h0.detach())
+
+        # Index hidden state of last time step
+        # out.size() --> 100, 28, 100
+        # out[:, -1, :] --> 100, 100 --> just want last time step hidden states! 
+        out = self.fc(out[:, -1, :]) 
+        # out.size() --> 100, 10
+        return out
+
+#  ██████ ███    ██ ███    ██ 
+# ██      ████   ██ ████   ██ 
+# ██      ██ ██  ██ ██ ██  ██ 
+# ██      ██  ██ ██ ██  ██ ██ 
+#  ██████ ██   ████ ██   ████ 
+
+class CNNModel(nn.Module):
+    def __init__(self):
+        super(CNNModel, self).__init__()
+
+        # Convolution 1
+        self.cnn1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=5, stride=1, padding=0)
+        self.relu1 = nn.ReLU()
+
+        # Max pool 1
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2)
+
+        # Convolution 2
+        self.cnn2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, stride=1, padding=0)
+        self.relu2 = nn.ReLU()
+
+        # Max pool 2
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2)
+
+        # Fully connected 1 (readout)
+        self.fc1 = nn.Linear(32 * 4 * 4, 10) 
+
+    def forward(self, x):
+        # Convolution 1
+        out = self.cnn1(x)
+        out = self.relu1(out)
+
+        # Max pool 1
+        out = self.maxpool1(out)
+
+        # Convolution 2 
+        out = self.cnn2(out)
+        out = self.relu2(out)
+
+        # Max pool 2 
+        out = self.maxpool2(out)
+
+        # Resize
+        # Original size: (100, 32, 7, 7)
+        # out.size(0): 100
+        # New out size: (100, 32*7*7)
+        out = out.view(out.size(0), -1)
+
+        # Linear function (readout)
+        out = self.fc1(out)
+
+        return out
+
+# ██      ███████ ███    ██ ███████ ████████ 
+# ██      ██      ████   ██ ██         ██    
+# ██      █████   ██ ██  ██ █████      ██    
+# ██      ██      ██  ██ ██ ██         ██    
+# ███████ ███████ ██   ████ ███████    ██   
+
+class lenet(nn.Module):
+    def __init__(self):
+        super(lenet, self).__init__()
+
+        self.conv1 = nn.Conv2d(1, 6, 3, padding=(2,2))
+        self.conv2 = nn.Conv2d(6, 16, 3)
+        # an affine operation: y = Wx + b
+        self.fc1 = nn.Linear(16 * 6 * 6, 120)  # 6*6 from image dimension
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        # Max pooling over a (2, 2) window
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        # If the size is a square you can only specify a single number
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        
+        return x
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+
+        for s in size:
+            num_features *= s
+        return num_features
+
+#  ██████ ██████  ███    ██ ███    ██ 
+# ██      ██   ██ ████   ██ ████   ██ 
+# ██      ██████  ██ ██  ██ ██ ██  ██ 
+# ██      ██   ██ ██  ██ ██ ██  ██ ██ 
+#  ██████ ██   ██ ██   ████ ██   ████
+
+
+
+# ███████ ███████ ██████        ██    ██  ██████  
+# ██      ██      ██   ██       ██    ██ ██    ██ 
+# █████   ███████ ██████  █████ ██    ██ ██    ██ 
+# ██           ██ ██             ██  ██  ██    ██ 
+# ███████ ███████ ██              ████    ██████ 
+
+
+
+#  ██████   █████  ███    ██ 
+# ██       ██   ██ ████   ██ 
+# ██   ███ ███████ ██ ██  ██ 
+# ██    ██ ██   ██ ██  ██ ██ 
+#  ██████  ██   ██ ██   ████
